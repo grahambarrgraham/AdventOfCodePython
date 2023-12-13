4f57967ffba0e83dc050925c93331caf309ff065
gbarr <graham.barr@skyscanner.net>
2023-12-13 08:35:26 +0000

    day12

--- 2023/day9.py
+++ 2023/day9.py
@@ -3,6 +3,18 @@ from pathlib import Path
 TEST_MODE = False
 
 
+def phase1(v):
+    all_diffs = [diffs(i) for i in v]
+    predicts = [predict_forward(d)[0][-1] for d in all_diffs]
+    return sum(predicts)
+
+
+def phase2(v):
+    all_diffs = [diffs(i) for i in v]
+    predicts = [predict_backward(d)[0][0] for d in all_diffs]
+    return sum(predicts)
+
+
 def predict_forward(s):
     pairs = [(s[i + 1], s[i]) for i in range(0, len(s) - 1)]
     for p in reversed(pairs):
@@ -37,24 +49,8 @@ def diff(s: list):
     return [(s[i + 1] - s[i]) for i in range(0, len(s) - 1)]
 
 
-def phase1(v):
-    all_diffs = [diffs(i) for i in v]
-    # print(all_diffs)
-    predicts = [predict_forward(d)[0][-1] for d in all_diffs]
-    # print(predicts)
-    return sum(predicts)
-
-
-def phase2(v):
-    all_diffs = [diffs(i) for i in v]
-    # print(all_diffs)
-    predicts = [predict_backward(d)[0][0] for d in all_diffs]
-    # print(predicts)
-    return sum(predicts)
-
-
 if __name__ == "__main__":
     with Path(__file__).parent.joinpath("input/day9_sample" if TEST_MODE else "input/day9").open() as f:
         values = [[int(x) for x in i.strip().split()] for i in f]
-        # print(f'Phase 1: {phase1(values.copy())}')
+        print(f'Phase 1: {phase1(values.copy())}')
         print(f'Phase 2: {phase2(values.copy())}')
